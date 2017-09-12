c8072ff9ee1eb1c43e9bbff63786d68696a338bf
ayushi <ayushi.jain@mtxb2b.com>
2017-09-12 11:18:05 +0530

    Code commit

---
+++ SampleTabBarApp/TabViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TabViewController.swift
+//  SampleTabBarApp
+//
+//  Created by Cloudzeg Laptop on 9/11/17.
+//  Copyright © 2017 Cloudzeg Laptop. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UITabBarController
+{
+   
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+       // var IIcons = [String]()
+       // IIcons.append("ic_star")
+      // let color = UIColor(colorLiteralRed: 14.0/255, green: 122.0/255, blue: 254.0/255, alpha: 1.0)
+        
+       // let selectedColor   = UIColor(red: 246.0/255.0, green: 155.0/255.0, blue: 13.0/255.0, alpha: 1.0)
+        //let unselectedColor = UIColor(red: 16.0/255.0, green: 224.0/255.0, blue: 223.0/255.0, alpha: 1.0)
+        
+       // UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: unselectedColor], for: .normal)
+      //  UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: selectedColor], for: .selected)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
